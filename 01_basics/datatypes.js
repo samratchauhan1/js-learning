51993349d8ece366bec80d7979b86fb81d5b82f1
Samrat Singh <samrattirwa@gmail.com>
2026-03-25 19:17:34 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+//treat all js code as nnewer version
+let name="hi"
+let age=18
+let islogged=false
+let state=null
+let states;//undefined
+
+//primitive data types
+
+//number 2 to thep power 53
+//bigint
+//string=>""
+//boolean => true /false
+//null=>standard value
+//undefined =>
+//symbol=>tofind uniqueness 
+
+//objects
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
